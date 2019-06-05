605c82c1934ac15414e614b57bf776aef575d266
Ryan Browne <brownetown7@gmail.com>
2019-06-05 22:07:32 +0100

    Adds Todos component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Todos from './components/Todos';
+
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <Todos />
     </div>
   );
 }
